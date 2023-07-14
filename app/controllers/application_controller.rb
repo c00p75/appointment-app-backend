79454da75561b26ac7dbbd7f86c4b26b521aeb32
veronica365 <naruth10@gmail.com>
2023-07-14 08:59:24 +0300

    Ensure authentication when the user tries to CUD requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::API
   before_action :update_allowed_parameters, if: :devise_controller?
 
+  skip_before_action :verify_authenticity_token, raise: false
+  before_action :authenticate_devise_api_token!, unless: [:devise_controller?, :get_request?]
+
+  def resource_class
+    User
+  end
+
   protected
 
   def update_allowed_parameters
@@ -8,5 +15,9 @@ class ApplicationController < ActionController::API
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :surname, :username, :email, :password)
     end
+  end
+
+  def get_request?
+    request.method == "GET"
   end
 end
