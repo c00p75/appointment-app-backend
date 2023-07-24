2f9ff9b5b9047c01ce724a4a7cf77057799c9f21
mossa <martin.ossa.8@gmail.com>
2023-07-24 12:07:58 +0200

    Fix linters

--- Gemfile
+++ Gemfile
@@ -43,12 +43,10 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
   gem 'dotenv-rails'
-  gem 'rubocop'
-  gem 'rspec-rails', '~> 5.0'
   gem 'factory_bot_rails'
+  gem 'rspec-rails', '~> 5.0'
+  gem 'rubocop'
   gem 'shoulda-matchers', require: false
-
-
 end
 
 group :development do
--- spec/models/motorcycle_spec.rb
+++ spec/models/motorcycle_spec.rb
@@ -12,39 +12,39 @@ RSpec.describe Motorcycle, type: :model do
       finance_fee: 1000,
       purchase_fee: 2000,
       amount_payable: 5000,
-      user: user
+      user:
     )
   end
 
   subject { motorcycle }
 
-  describe "validations" do
+  describe 'validations' do
     it 'is valid with valid attributes' do
       expect(subject).to be_valid
     end
 
-    context "model" do
+    context 'model' do
       it 'is not valid without a model' do
         subject.model = nil
         expect(subject).not_to be_valid
       end
     end
 
-    context "description" do
+    context 'description' do
       it 'is not valid without a description' do
         subject.description = nil
         expect(subject).not_to be_valid
       end
     end
 
-    context "photo" do
+    context 'photo' do
       it 'is not valid without a photo' do
         subject.photo = nil
         expect(subject).not_to be_valid
       end
     end
 
-    context "duration" do
+    context 'duration' do
       it 'is not valid without a duration' do
         subject.duration = nil
         expect(subject).not_to be_valid
@@ -56,7 +56,7 @@ RSpec.describe Motorcycle, type: :model do
       end
     end
 
-    context "finance_fee" do
+    context 'finance_fee' do
       it 'is not valid without a finance_fee' do
         subject.finance_fee = nil
         expect(subject).not_to be_valid
@@ -68,7 +68,7 @@ RSpec.describe Motorcycle, type: :model do
       end
     end
 
-    context "purchase_fee" do
+    context 'purchase_fee' do
       it 'is not valid without a purchase_fee' do
         subject.purchase_fee = nil
         expect(subject).not_to be_valid
@@ -80,7 +80,7 @@ RSpec.describe Motorcycle, type: :model do
       end
     end
 
-    context "amount_payable" do
+    context 'amount_payable' do
       it 'is not valid without an amount_payable' do
         subject.amount_payable = nil
         expect(subject).not_to be_valid
@@ -93,7 +93,7 @@ RSpec.describe Motorcycle, type: :model do
     end
   end
 
-  describe "associations" do
+  describe 'associations' do
     it { should belong_to(:user).optional }
 
     it { should have_many(:reservations).dependent(:destroy) }
